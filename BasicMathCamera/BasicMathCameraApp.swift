f2ba4ff3a0655ebc934613c6453aa4f65475cd9a
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2023-08-30 21:15:54 +0800

    Initial Commit

---
+++ BasicMathCamera/BasicMathCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicMathCameraApp.swift
+//  BasicMathCamera
+//
+//  Created by Jaime Jazareno IV on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct BasicMathCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
